638c8901541aac45ec6ca4136fb550d0d1c48c29
newxan <olundh@kth.se>
2015-03-19 13:57:05 +0100

    added vagrant stuff

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -3,5 +3,11 @@ add-apt-repository ppa:chris-lea/node.js
 apt-get update
 apt-get install -y python-software-properties python g++ make
 apt-get install -y nodejs
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y -q mysql-server
 npm -g install sails
-sails new testProject
+git clone https://github.com:frias0/leet_chat.git
+cd leet_chat
+mysql -u root < db.sql
+cd backend
+sails lift
