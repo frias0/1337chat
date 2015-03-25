5fdf93ffc826a2649d19c7b06146c40b8d051d23
newxan <olundh@kth.se>
2015-03-25 11:03:09 +0100

    added prpvoder output

--- backend/api/services/passport.js
+++ backend/api/services/passport.js
@@ -247,11 +247,12 @@ passport.callback = function (req, res, next) {
     if (action === 'disconnect' && req.user) {
       this.disconnect(req, res, next) ;
     } else {
-      console.log("HULLO");
+      //console.log("HULLO");
       // The provider will redirect the user to this URL after approval. Finish
       // the authentication process by attempting to obtain an access token. If
       // access was granted, the user will be logged in. Otherwise, authentication
       // has failed.
+      console.log(provider);
       this.authenticate(provider, next)(req, res, req.next);
     }
   }
