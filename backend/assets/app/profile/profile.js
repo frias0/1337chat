3fe8a7ebda857b4deedad3a6693c6d0e3b3a310b
newxan <olundh@kth.se>
2015-03-28 10:43:39 +0100

    started adding angular functionality to profile page

--- backend/assets/app/profile/profile.js
+++ backend/assets/app/profile/profile.js
@@ -9,8 +9,27 @@
         scope.description = attrs.description;
         scope.groups = angular.fromJson(attrs.groups);
       },
-      controller: ['$scope', '$log', function ($scope, $log) {
-        
+      controller: ['$scope', '$log', '$http', function ($scope, $log, $http) {
+        $scope.getGroups = function(){
+          $http.get('/user/groups').success(function(data){
+            console.log("fetched groups", data[0]);
+            $scope.groups = data;
+          });
+        };
+        $scope.getFriends = function(){
+          $http.get('/user/friends').success(function(data){
+            console.log("fetched friends", data[0]);
+            $scope.friends = data;
+          });
+        };
+        $scope.createGroup = function(){
+          $http.post('/groups/create').success(function(data){
+            console.log("group created", data[0]);
+            //$scope.friends = data;
+          }).error(function(data){
+            console.log(eror creating group, data);
+          });
+        };
       }],
       controllerAs: 'profileCtrl'
     };
