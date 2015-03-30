88752dded44525c0e6f18b16e3532b2279f08122
Andreas Hallberg <anhallbe@gmail.com>
2015-03-30 10:29:54 +0000

    navbar display friend list

--- backend/assets/app/navbar/navbar.js
+++ backend/assets/app/navbar/navbar.js
@@ -8,11 +8,26 @@
         console.log(elem);
         scope.pagename = attrs.title;
         scope.context = attrs.context;
+        scope.friends = [];
       },
-      controller: ['$scope', '$log', function ($scope, $log) {
+      controller: ['$scope', '$log', '$http', function ($scope, $log, $http) {
         $scope.toggleSidebar = function() {
           $("#wrapper").toggleClass("toggled");
         };
+
+        $scope.getFriends = function() {
+          $log.info("getting http user/, change to user/id");
+          $http.get('/user').success(function(data) {
+            $log.info("Data[0].friends: ");
+            $log.info(data[0].friends);
+            $scope.friends = data[0].friends;
+            for(var i=0; i<$scope.friends.length; i++) {
+              $log.info($scope.friends[i]);
+            }
+          });
+        };
+
+        $scope.getFriends();
       }],
       controllerAs: 'navbarCtrl'
     };
